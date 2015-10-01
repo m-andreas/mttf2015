1855b7c6335f537ee33bcc654773321369b4b3f2
m-andreas <mar.andreas@yahoo.de>
2015-10-01 15:58:44 +0200

    precompiled

---
+++ public/assets/drivers-ab46e109fad685c5c10e2eebf4d089ca43a67d460fc9118d2866f208019727cb.js
@@ -0,0 +1,1 @@
+(function(){jQuery(function(){return $("#drivers").DataTable({responsive:!1}),$("#driver_date_of_birth").datepicker({dateFormat:"yy-mm-dd"}),$("#driver_entry_date").datepicker({dateFormat:"yy-mm-dd"}),$("#driver_exit_date").datepicker({dateFormat:"yy-mm-dd"})})}).call(this);
