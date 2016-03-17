26e31c9f69a8c89546759e157770babea68026c9
m-andreas <mar.andreas@yahoo.de>
2016-03-17 13:28:13 +0100

    test show address

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -8,4 +8,11 @@ class AddressTest < ActiveSupport::TestCase
   test "get_full_address" do
     assert addresses(:one).complete_address.is_a? String
   end
+
+  test "show address" do
+  	assert_equal "Mondstrasse Wien", addresses(:one).show_address
+	addresses(:one).display_name = "Mond"
+	addresses(:one).save
+  	assert_equal "Mond", addresses(:one).show_address
+  end
 end
